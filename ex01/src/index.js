04ba3775a84e263f4a43c35d724baec493c8a354
mstfhlc <mustafahalici30@gmail.com>
2021-03-16 05:38:05 +0100

    creating ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// Write code below this line
+function reusableFunction(msg) {
+
+    
+    var msg="Hi Padawan";
+    console.log(msg);
+}
+// Write code below this line
+module.exports =reusableFunction;
